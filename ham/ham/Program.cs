f3947c13f98121e8b361efbce298b04d2ce282d7
anhduongseodzt-ui <anhduongseodzt@gmail.com>
2025-11-15 16:59:07 +0700

    lan3

--- ham/ham/Program.cs
+++ ham/ham/Program.cs
@@ -10,7 +10,7 @@ namespace ham
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("anh yeu em");
+            Console.WriteLine("anh yeu em nhieu lam");
         }
     }
 }
